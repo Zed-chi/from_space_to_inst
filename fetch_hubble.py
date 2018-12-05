f5a653aed99efe1c7897141d389239bc51c8c459
Zed-chi <zed-chi@mail.ru>
2018-12-06 01:16:28 +0300

    Убрал import * и main()

--- fetch_hubble.py
+++ fetch_hubble.py
@@ -1,18 +1,19 @@
 ### http://hubblesite.org/api/documentation
-from utils import *
+from utils import fetch_and_save_images
+import requests
 
 
-def fetch_ids_from_image_collection(collection):
-    url = "http://hubblesite.org/api/v3/images/"+collection
+def fetch_ids_from_image_collection(image_collection):
+    url = "http://hubblesite.org/api/v3/images/"+image_collection
     return tuple(
-        map(lambda x: x["id"], fetch_json(url))
+        map(lambda x: x["id"], requests.get(url).json())
     )
 
 
 def fetch_hubble_images_links_by_id(id):
     url = "http://hubblesite.org/api/v3/image/"+str(id)
     return tuple(
-        map(lambda x: x["file_url"], fetch_json(url)["image_files"])
+        map(lambda x: x["file_url"], requests.get(url).json()["image_files"])
     )
 
 
@@ -21,18 +22,24 @@ def fetch_hubble_images_by_id(id, image_dir):
     fetch_and_save_images(hubble_imagelinks, image_dir)
 
 
-def fetch_hubble_images_by_collection(collection="news", image_dir="temp"):
-    ids = fetch_ids_from_image_collection(collection)
-    for id in ids:
-        fetch_hubble_images_by_id(id, image_dir)
-    print("→ Done")
-
-
-def main(image_dir="temp"):
-    image_dir = input("► Type destination dir: ")
-    collection = input("► Type collection you want to download: ")
-    fetch_hubble_images_by_collection(collection, image_dir)
+def fetch_hubble_images_by_collection(image_collection="news", image_dir="temp"):
+    ids = fetch_ids_from_image_collection(image_collection)
+    if ids:
+        for id in ids:
+            fetch_hubble_images_by_id(id, image_dir)
+        return True
+    else:
+        return None
+    
 
 
 if __name__ == "__main__":
-    main()
+    img_dir = input("► Type destination dir: ")
+    img_collection = input("► Type collection you want to download: ")
+    try:
+        if fetch_hubble_images_by_collection(img_collection, img_dir):
+            print("→ Done")
+        else:
+            print("No images")
+    except Exception as e:
+        print(e)
