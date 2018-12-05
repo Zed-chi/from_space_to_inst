3179d2c2cc6fe415b911f4ae03464882546a029d
Zed-chi <zed-chi@mail.ru>
2018-12-06 01:14:02 +0300

    Убрал лишние функции

--- utils.py
+++ utils.py
@@ -8,27 +8,12 @@ def load_and_save_image(url, path):
         f.write(r.content)
 
 
-def fetch_json(url):
-    res = requests.get(url)
-    return res.json()
-
-
-def get_name_from_url(url):
-    return url.split("/")[-1]
-
-
-def get_path(image_dir, name):
-    return os.path.join(image_dir, name)
-
-
 def fetch_and_save_images(links, image_dir):
     if not os.path.exists(image_dir):
         os.makedirs(image_dir)
     for link in links:
-        name = get_name_from_url(link)
+        name = link.split("/")[-1]
         # из-за большого размера файла ввел возможность выбора
         if input("→ Load {}?(Y/N): ".format(name)).upper() == "Y":
-            path = get_path(image_dir, name)
-            print("\t╟ loading {}".format(link))
+            path = os.path.join(image_dir, name)
             load_and_save_image(link, path)
-            print("\t╚ done")
