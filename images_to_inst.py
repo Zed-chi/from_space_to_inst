8b62b790c720fb95f1d43107990ff55240f30f0a
Zed-chi <zed-chi@mail.ru>
2018-12-06 00:07:44 +0300

    Вывел доступ к логину/паролю в .env файл

--- images_to_inst.py
+++ images_to_inst.py
@@ -1,5 +1,6 @@
 import os
 from instabot import Bot
+from dotenv import load_dotenv
 from fetch_hubble import fetch_hubble_images_by_collection
 from fetch_spacex import fetch_spacex_last_launch_images
 
@@ -12,12 +13,15 @@ def upload_images_to_instagram(user, image_dir):
 
 
 def main():
+    load_dotenv()
+    username = os.getenv("username")
+    password = os.getenv("password")
     image_dir = "images"
     hubble_collection = "spacecraft"
     fetch_spacex_last_launch_images(image_dir)
     fetch_hubble_images_by_collection(hubble_collection, image_dir)
     user = Bot()
-    user.login()
+    user.login(username=username, password=password)
     upload_images_to_instagram(user, image_dir)
 
 
