7aa0c3a8eeb12957256731002a0818257e72c563
Zed-chi <zed-chi@mail.ru>
2018-12-06 01:15:38 +0300

    Убрал import * и main()

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -1,17 +1,25 @@
 ### https://documenter.getpostman.com/view/2025350/RWaEzAiG#07a29989-38e3-47fb-9f64-c132b5842ff0
-from utils import *
+from utils import fetch_and_save_images
+import requests
 
 
 def fetch_spacex_last_launch_images(image_dir="temp"):
     latest_launch_url = "https://api.spacexdata.com/v3/launches/latest"
-    image_links = fetch_json(latest_launch_url)["links"]["flickr_images"]
-    fetch_and_save_images(image_links, image_dir)
-
-
-def main():
-    image_dir = input("► Type destination dir: ")
-    fetch_spacex_last_launch_images(image_dir)
+    image_links = requests.get(latest_launch_url).json()["links"]["flickr_images"]
+    if image_links:
+        fetch_and_save_images(image_links, image_dir)
+        return True
+    else:
+        return None
+    
 
 
 if __name__ == "__main__":
-    main()
+    img_dir = input("► Type destination dir: ")
+    try:
+        if fetch_spacex_last_launch_images(img_dir):
+            print("→ Done")
+        else:
+            print("→ No images")
+    except Exception as e:
+        print(e)
